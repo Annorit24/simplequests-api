f4f3526ca146bf6c61b5bd8484bca90d4f239df7
Annorit24 <Annorit24@gmail.com>
2020-01-22 09:59:11 +0100

    Add valideConditions field to action and change system to get condition value

--- src/main/java/xyz/annorit24/simplequestsapi/actions/Action.java
+++ src/main/java/xyz/annorit24/simplequestsapi/actions/Action.java
@@ -2,12 +2,27 @@ package xyz.annorit24.simplequestsapi.actions;
 
 import org.bukkit.event.Event;
 
+import java.util.List;
+
 /**
  * @author Annorit24
  * Created on 21/01/2020
  */
 public abstract class Action {
 
+    private List<Integer> validConditions;
+
+    public Action(List<Integer> validConditions) {
+        this.validConditions = validConditions;
+    }
+
     public abstract void call(Event event);
 
+    public List<Integer> getValidConditions() {
+        return validConditions;
+    }
+
+    public void setValidConditions(List<Integer> validConditions) {
+        this.validConditions = validConditions;
+    }
 }
--- src/main/java/xyz/annorit24/simplequestsapi/condition/Condition.java
+++ src/main/java/xyz/annorit24/simplequestsapi/condition/Condition.java
@@ -4,21 +4,8 @@ import org.bukkit.event.Event;
 
 public abstract class Condition {
 
-    protected Boolean conditionValue;
-
-    public Condition() {
-        this.conditionValue = false;
-    }
-
-    public abstract void call(Event event);
+    public abstract boolean call(Event event);
 
     public abstract void manageInvalidCondition(Event event);
 
-    public Boolean getConditionValue() {
-        return conditionValue;
-    }
-
-    public void setConditionValue(Boolean conditionValue) {
-        this.conditionValue = conditionValue;
-    }
 }
