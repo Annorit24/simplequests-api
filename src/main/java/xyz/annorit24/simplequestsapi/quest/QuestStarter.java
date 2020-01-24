251a02ad04b5dc98e49bd46823e031fb08ec39ec
Annorit24 <Annorit24@gmail.com>
2020-01-24 13:12:34 +0100

    Add event parameter

--- src/main/java/xyz/annorit24/simplequestsapi/quest/QuestStarter.java
+++ src/main/java/xyz/annorit24/simplequestsapi/quest/QuestStarter.java
@@ -1,5 +1,7 @@
 package xyz.annorit24.simplequestsapi.quest;
 
+import java.awt.*;
+
 /**
  * @author Annorit24
  * Created on 20/01/2020
@@ -9,6 +11,6 @@ public abstract class QuestStarter {
     /**
      * Start the quest
      */
-    public abstract void start();
+    public abstract void start(Event event);
 
 }
