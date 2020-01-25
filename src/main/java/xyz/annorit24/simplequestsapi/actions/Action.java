526ab2b34bdb4d2b836657b8ed634757a3f9ea4a
Annorit24 <Annorit24@gmail.com>
2020-01-25 10:19:16 +0100

    Add method to set finish statu

--- src/main/java/xyz/annorit24/simplequestsapi/actions/Action.java
+++ src/main/java/xyz/annorit24/simplequestsapi/actions/Action.java
@@ -56,4 +56,8 @@ public abstract class Action {
     public boolean isFinish() {
         return finish;
     }
+
+    public void setFinish(boolean finish) {
+        this.finish = finish;
+    }
 }
