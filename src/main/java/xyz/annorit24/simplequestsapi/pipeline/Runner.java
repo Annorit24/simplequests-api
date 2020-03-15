6de00efe09b78afa40ff79fec4677ca7ca44a7f9
Annorit24 <Annorit24@gmail.com>
2020-03-15 10:52:17 +0100

    add debug messages

--- src/main/java/xyz/annorit24/simplequestsapi/pipeline/Runner.java
+++ src/main/java/xyz/annorit24/simplequestsapi/pipeline/Runner.java
@@ -3,6 +3,8 @@ package xyz.annorit24.simplequestsapi.pipeline;
 import xyz.annorit24.simplequestsapi.quest.Container;
 import xyz.annorit24.simplequestsapi.utils.Callback;
 
+import java.util.Map;
+
 /**
  * @author Annorit24
  * Created on 12/02/2020
@@ -23,18 +25,30 @@ public abstract class Runner implements IQuestEventReader {
         process(container);
         System.out.println("PROCESS AFTER");
         return () -> {
+            System.out.println("CALLING NEXT RUNNER");
             Integer i = container.getIndexPosition();
-            System.out.println(i);
+            System.out.println("current index : "+i);
             i++;
             container.setIndexPosition(i);
+            System.out.println("next index : "+i);
+
+            int j = 0;
+
+            for (Map.Entry<String, Runner> entry : pipeline.getRunners().entrySet()) {
+                String s = entry.getKey();
+                Runner value = entry.getValue();
+                System.out.println(j+" : "+value.getSlug());
+                j++;
+            }
 
             Runner runner = pipeline.getRunners().getValue(i);
             if(runner == null)return null;
-            Callback c = runner.read(container);
+
             System.out.println(runner.getSlug());
+            Callback c = runner.read(container);
             c.run();
 
-            System.out.println(i);
+            System.out.println("FINISH");
 
             return null;
         };
